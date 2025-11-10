b05b664cc4b57d8b059c4672bae6855c47b8d54b
Sergey Lezhnev <splezhnev@avito.ru>
2025-11-10 23:27:09 +0500

    Working tests

--- lua/theprimeagen/lazy/neotest.lua
+++ lua/theprimeagen/lazy/neotest.lua
@@ -1,75 +1,72 @@
 return {
-    "nvim-neotest/neotest",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-neotest/nvim-nio",
-        "antoinemadec/FixCursorHold.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-neotest/neotest-go",
-    },
-    config = function()
-        local neotest = require("neotest")
-        local neotest_go = require("neotest-go")
-        local lib = require("neotest.lib")
-
-        local match_go_root = lib.files.match_root_pattern("go.mod", "go.work", ".git")
-
-        neotest.setup({
-            level = vim.log.levels.DEBUG,
-            discovery = { enabled = true },
-            adapters = {
-                neotest_go({
-                    root = function(path)
-                        return match_go_root(path) or vim.loop.cwd()
-                    end,
-                    recursive_run = true,
-                    level = vim.log.levels.DEBUG,
-                    experimental = { test_table = true },
-                    args = { "-count=1", "-v" },
-                }),
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            {
+                "nvim-treesitter/nvim-treesitter", -- Optional, but recommended
+                branch = "main",                   -- NOTE; not the master branch!
+                build = function()
+                    vim.cmd(":TSUpdate go")
+                end,
             },
-        })
+            {
+                "fredrikaverpil/neotest-golang",
+                version = "*",                                                              -- Optional, but recommended; track releases
+                build = function()
+                    vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait() -- Optional, but recommended
+                end,
+            },
+        },
+        config = function()
+            local config = {
+                runner = "gotestsum", -- Optional, but recommended
+            }
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-golang")(config),
+                }
+            })
 
-        local function run_nearest_or_file()
-            local tree = neotest.run.get_tree_from_args(nil, false)
-            if tree then
-                neotest.run.run()
-            else
-                neotest.run.run(vim.fn.expand("%"))
-            end
-        end
+            -- биндинги
+            vim.keymap.set("n", "<leader>tr", function() require("neotest").run.run() end, { desc = "Test: run nearest" })
+            vim.keymap.set("n", "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end,
+                { desc = "Test: run file" })
+            vim.keymap.set("n", "<leader>ts", function() require("neotest").run.run(vim.fn.getcwd()) end,
+                { desc = "Test: run suite (cwd)" })
+            vim.keymap.set("n", "<leader>tp", function() require("neotest").output_panel.toggle() end,
+                { desc = "Test: output panel" })
+            vim.keymap.set("n", "<leader>to", function()
+                local nt = require("neotest"); local id = nt.run.get_last_run()
+                nt.output.open({ enter = true, last_run = id ~= nil })
+            end, { desc = "Test: open last output" })
 
-        vim.keymap.set("n", "<leader>tr", run_nearest_or_file, { desc = "Test: run nearest (fallback file)" })
-        vim.keymap.set("n", "<leader>td", function()
-            local tree = neotest.run.get_tree_from_args(nil, false)
-            if tree then
-                neotest.run.run({ strategy = "dap" })
-            else
-                neotest.run.run({ vim.fn.expand("%"), strategy = "dap" })
-            end
-        end, { desc = "Test: debug nearest (fallback file)" })
+            vim.api.nvim_create_user_command("NeoTestNearestDebug", function()
+                local nt = require("neotest")
+                local path = vim.fn.expand("%:p")
+                local tree = nt.state.positions(path)
+                if not tree then
+                    print("no positions (treesitter didn't parse this file)")
+                    return
+                end
+                local row = vim.api.nvim_win_get_cursor(0)[1] - 1 -- 0-based
+                local nearest
 
-        vim.keymap.set("n", "<leader>tf", function()
-            neotest.run.run(vim.fn.expand("%"))
-        end, { desc = "Test: run current file" })
+                for _, node in ipairs(tree:to_list()) do
+                    -- интересуют только test / namespace (subtests и т.п.)
+                    if node.type == "test" or node.type == "namespace" then
+                        local r = node.range -- { start_row, start_col, end_row, end_col }
+                        if r and r[1] <= row and row <= r[3] then
+                            nearest = node
+                            break
+                        end
+                    end
+                end
 
-        vim.keymap.set("n", "<leader>tF", function()
-            neotest.run.run({ vim.fn.expand("%"), strategy = "dap" })
-        end, { desc = "Test: debug current file" })
+                print(nearest and (nearest.id .. "  " .. nearest.type .. "  " .. nearest.name) or "nil")
+            end, {})
+        end,
 
-        vim.keymap.set("n", "<leader>ts", function()
-            neotest.run.run(vim.fn.getcwd())
-        end, { desc = "Test: run suite (cwd)" })
-
-        vim.keymap.set("n", "<leader>tv", neotest.summary.toggle, { desc = "Test: toggle summary" })
-
-        vim.keymap.set("n", "<leader>to", function()
-            local position_id = neotest.run.get_last_run()
-            if position_id then
-                neotest.output.open({ enter = true, last_run = true })
-            else
-                neotest.output.open({ enter = true })
-            end
-        end, { desc = "Test: open last output" })
-    end,
-}
+    } }
